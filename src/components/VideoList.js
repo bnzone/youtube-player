6d5d00c01a04855b3bc6f837b7f97865de80597e
Igor Shevchenko <hello.igorshevchenko@gmail.com>
2021-01-18 23:17:19 -0500

    fixed the key error in console

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,14 +1,18 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-// descturcured videos from props
-const VideoList = ({ videos }) => {
-  //props.videos
-
+// descturcured videos and ovVideoSelect passes as props from App.js
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
